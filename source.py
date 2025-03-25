210c7cf56773c996da52425f09c697cc10666162
script <mmoatazmm8@gmail.com>
2025-03-25 20:15:28 +0200

    simplify logging in check_boss function by removing character limit check

--- source.py
+++ source.py
@@ -315,11 +315,7 @@ def find_correct_words(words):
 def check_boss(text, gui, mode):
     text = text.lower()
     if text != "" and len(text) <= 500:
-
-        if len(text) > 80:
-            gui.log(text[:80])
-        else:
-            gui.log(text)
+        gui.log(text)
 
         # Check for keywords
         clean_text = text
